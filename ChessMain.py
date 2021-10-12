27caa47d7e168748b81d7c1583a7c4c111d41d35
jeloneklibera <jan.piotrowski00@gmail.com>
2021-10-12 18:14:02 +0200

    Commit #7: Stworzenie funkcji load_images() odpowiedzialnej za zaladowanie obrazkow figur szachowych

--- ChessMain.py
+++ ChessMain.py
@@ -1,9 +1,25 @@
 """Glowny plik sterownika. Odpowiedzialny za obsługę akcji użytkownika i wyświetlanie aktualnego stanu gry."""
 
-import pygame as p 
+import pygame as pg 
 import Engine
 
 WIDTH = HEIGHT = 512
+DIMENSION = 8 #Wymiary szachownicy to 8x8 pól
+SQ_SIZE = WIDTH // DIMENSION #Rozmiar pojedynczego pola: 512/8=64
+MAX_FPS = 30 #Parametr animacji, maksymalna liczba klatek na sekundę
+IMAGES = {}
+
+"""
+Inicjalizuje globalny słownik obrazów. Zostanie wywołane tylko jeden raz w funkcji main() ze względu na oszczędność zasobów 
+(zasobochłonne wielkokrotne ładowanie obrazków)
+"""
+
+def load_images():
+    pieces = ["wp", "wR", "wN", "wB", "wQ", "wK", "bp", "bR", "bN", "bB", "bQ", "bK"]
+    for piece in pieces:
+        IMAGES[piece] = pg.transform.scale(pg.image.load("resources/" + piece + ".png"), SQ_SIZE, SQ_SIZE)
+
+
 
 
 
