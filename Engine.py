e9f18271ec5af8fc6d9af0af2b2c10f7475240b9
jeloneklibera <jan.piotrowski00@gmail.com>
2021-10-12 17:33:02 +0200

    inicjalizacja planszy przy pomocy 8x8 2d listy

--- Engine.py
+++ Engine.py
@@ -1,3 +1,20 @@
 """Klasa odpowiedzialna za przechowywanie wszystkich informacji dotyczących stanu gry
 Odpowiada również za określanie legalnych ruchów przy danym stanie gry
+Zawiera log z wykonanymi ruchami
 """
+
+class GameState():
+    def __init__(self):
+        #Reprezentacja planszy przy pomocy 8x8 2d listy, każdy element listy składa się z 2 znaków
+        #Pierwszy znak reprezentuje kolor danej figury: b-black, w-white
+        #Drugi znak reprezentuje rodzaj figury: R - Rook, N - Knight, B - Bishop, Q - Queen, K - King, p - pawn
+        # "--" oznacza niezajęte pole na szachownicy
+        self.board = [
+            ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"], 
+            ["bp", "bp", "bp", "bp", "bp", "bp", "bp", "bp"],
+            ["--", "--", "--", "--","--", "--","--", "--"],
+            ["--", "--", "--", "--","--", "--","--", "--"],
+            ["--", "--", "--", "--","--", "--","--", "--"],
+            ["--", "--", "--", "--","--", "--","--", "--"],
+            ["wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp"],
+            ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]]
